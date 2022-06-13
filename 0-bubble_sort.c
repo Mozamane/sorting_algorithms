2b64312993c489deb9c863e38ae86bfc05a49731
Mozamane Baloyi <MBaloyi@SINGULAR.CO.ZA>
2022-06-13 15:04:05 +0200

    Sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,25 @@
 #include "sort.h"
-
 /**
- * swap_values - swaps 2 values in an array of ints
- *
- * @array: the array of ints
- * @i1: index of first value
- * @i2: index of 2nd value
- *
- * Return: the array with value
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
-
-void swap_values(int **array, ssize_t i1, ssize_t i2)
-{
-	int tmp;
-
-	tmp = (*array)[i1];
-	(*array)[i1] = (*array)[i2];
-	(*array)[i2] = tmp;
-}
-
-/**
- * bubble_sort - sorts an array of integers using bubble sort
- *
- * @array: the array of integers
- * @size: the size of the array
- */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t n = size, new_n, i;
 
-	while (n > 1)
-	{
-		new_n = 0;
-		for (i = 1; i <= n - 1; i++)
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				swap_values(&array, i - 1, i);
-				print_array(array, size);
-				new_n = i;
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		n = new_n;
-	}
 }
